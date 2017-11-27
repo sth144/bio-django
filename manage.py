0968bf4e6c842facb2dc5854f1a5e7c4323bba86
Sean Hinds <sthinds144@gmail.com>
2017-11-26 22:35:55 -0600

    changed color

--- manage.py
+++ manage.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
     from django.core.management import execute_from_command_line
-
+    print("hello")
     execute_from_command_line(sys.argv)
