48f27cf1207a876bef1fe1257b953b73b532346b
Sean Hinds <sthinds144@gmail.com>
2017-11-27 22:38:16 -0600

    hope

--- form/models.py
+++ form/models.py
@@ -2,11 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-#from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Blurb(models.Model):
 	post = models.CharField(max_length=500)
-	user = models.ForeignKey(User) # FIX
+	user = models.ForeignKey(User, default=1) # FIX
 	date = models.DateTimeField(auto_now=True)
