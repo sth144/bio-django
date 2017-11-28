bdd5887a4507097a20df9b92229cad8e70c27cf6
Sean Hinds <sthinds144@gmail.com>
2017-11-27 20:46:15 -0600

    tryin

--- form/models.py
+++ form/models.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 from django.contrib.auth.models import User
-user = User.objects.get(id=user_id)
 
 # Create your models here.
 
