1083d022ad548c25b0f7d2280327fd3d68f1424a
Sean Hinds <sthinds144@gmail.com>
2017-11-27 20:49:47 -0600

    tryin here

--- form/models.py
+++ form/models.py
@@ -1,12 +1,12 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import AnonymousUser, User
 from django.db import models
 
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Blurb(models.Model):
 	post = models.CharField(max_length=500)
-	user = models.ForeignKey(User)
+	user = AnonymousUser()
 	date = models.DateTimeField(auto_now=True)
