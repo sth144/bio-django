60d5318266a7f3b4ec24c1d350056830bf98c7b7
Sean Hinds <sthinds144@gmail.com>
2017-12-09 18:45:44 -0600

    try

--- Data/views.py
+++ Data/views.py
@@ -10,7 +10,7 @@ def db(request):
     RNASeqs = RNASeq.objects.all()
     PeptideSeqs = PeptideSeq.objects.all()
 
-    return render(request, 'app/data/templates/db.html', {
+    return render(request, 'Data/templates/db.html', {
         'DNASeqs': DNASeqs,
         'RNASeqs': RNASeqs,
         'PeptideSeqs': PeptideSeqs
