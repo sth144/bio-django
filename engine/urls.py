9c4336664463ebb66e9098e52a6738db3bb8253c
Sean Hinds <sthinds144@gmail.com>
2017-12-09 18:39:33 -0600

    try

--- engine/urls.py
+++ engine/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 import hello.views
 import form.views
 import ReverseComplement.views                      # Had to capitalize for Heroku
+import Data.views                                   # Had to capitalize for Heroku
 
 # Examples:
 # url(r'^$', 'engine.views.home', name='home'),
